c1985fd004ffa1a3b5e965c482726122ac6cfadf
tavolafourcade <tavolafourcade@gmail.com>
2025-01-10 22:51:32 -0500

    feat(navbar): implement Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="bg-grey-background flex justify-between px-6 py-5 sm:px-6 lg:px-32 text-2xl font-bold text-grey-subtext">
+      <Link href={"/"}>
+        <p className="cursor-pointer">GamerShop</p>
+      </Link>
+      <Link href={"cart"}>
+        <Image
+          src="/assets/icons/shoppingCart.svg"
+          alt="shopping icon"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
