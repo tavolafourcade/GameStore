71d2ed5d6288686f20f45d42238d9049a0caa91d
tavolafourcade <tavolafourcade@gmail.com>
2025-01-12 02:18:36 -0500

    feat(suspense): implement React Suspense for data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import Catalog from "@/components/Catalog";
 import { availableFilters } from "@/utils/endpoint";
 import { useSearchParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 
 const Page = () => {
   const searchParams = useSearchParams();
@@ -27,31 +27,34 @@ const Page = () => {
   }
 
   return (
-    <section className="flex flex-col">
-      <div className="px-6 py-8 gap-8 sm:py-12 sm:px-6 lg:px-32 flex flex-col sm:gap-12 w-full">
-        <p className="text-grey-medium text-4xl font-bold">Top Sellers</p>
-        <div className="w-full justify-end flex gap-6 items-center">
-          <label className="font-bold">Genre</label>
-          <div className="h-[22px] border border-grey-medium"></div>
-          <select
-            name="genere"
-            className="p-4 w-56 bg-white"
-            value={selectGenre}
-            onChange={handleCategory}
-          >
-            <option value="all">All</option>
-            {availableFilters.map((genre) => (
-              <option key={genre} value={genre}>
-                {genre}
-              </option>
-            ))}
-          </select>
+    <Suspense fallback={<div>Loading catalog...</div>}>
+      <section className="flex flex-col">
+        <div className="px-6 py-8 gap-8 sm:py-12 sm:px-6 lg:px-32 flex flex-col sm:gap-12 w-full">
+          <p className="text-grey-medium text-4xl font-bold">Top Sellers</p>
+          <div className="w-full justify-end flex gap-6 items-center">
+            <label className="font-bold">Genre</label>
+            <div className="h-[22px] border border-grey-medium"></div>
+            <select
+              name="genere"
+              className="p-4 w-56 bg-white"
+              value={selectGenre}
+              onChange={handleCategory}
+            >
+              <option value="all">All</option>
+              {availableFilters.map((genre) => (
+                <option key={genre} value={genre}>
+                  {genre}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-      </div>
-      <div className="flex-1">
-        <Catalog genre={selectGenre} />
-      </div>
-    </section>
+        <div className="flex-1">
+          <Catalog genre={selectGenre} />
+        </div>
+      </section>
+    </Suspense>
+
   );
 };
 
